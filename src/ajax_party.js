033360bfff8e4a3dbdea8e8065b6108dc0e61c5d
Kris <igg@six.(none)>
2008-12-08 15:54:37 -0500

    Refactored the create method. Moved all shared method into the Ajax.Party.Utils.Methods namespace. Added a couple more tests.

--- src/ajax_party.js
+++ src/ajax_party.js
@@ -1,9 +1,13 @@
 // It's An AJAX PARTY!!
+if (!Prototype) {
+  throw("ajax_party.js requires including the prototype.js Library.");
+}
+
 Ajax.Party = Class.create(Ajax.Request, {
   
   getBaseRequestOptions: function(options) {
     var request_options = {}
-    Object.extend(request_options, Object.clone(Ajax.Party.Base.defaultOptions));
+    Object.extend(request_options, Object.clone(Ajax.Party.class_default_options));
     if (options) {
       request_options.parameters = Object.extend(Object.clone(request_options.parameters), options.parameters);
       delete options.parameters;
@@ -17,51 +21,46 @@ Ajax.Party = Class.create(Ajax.Request, {
   } 
 });
 
-Ajax.Party.Base = {};
-Ajax.Party.Base.defaultOptions = {
-  parameters: {},
-  onSuccess: Prototype.emptyFunction,
-  onFailure: Prototype.emptyFunction  
-},
-
-Ajax.Party.create = function(class_name, options, class_default_options) {
-  var default_options = {
-    method: 'post',
-    parameters: {},
-    onSuccess: Prototype.emptyFunction,
-    onFailure: Prototype.emptyFunction      
-  }
-  
-  this[class_name] = Class.create(this, {
-    default_options: Object.extend(default_options, options),
-    
-    initialize: function($super, url, parameters, callback, options) {
-      $super(url, this.getRequestOptions(parameters, callback, options));          
-    }
-  });
-  
-  this[class_name].addMethods(Ajax.Party.Util.Methods);  
-  this[class_name].defaultOptions = (class_default_options || {});
-  this[class_name].create = this.create;
-}
-
 Ajax.Party.Util = {};
 Ajax.Party.Util.Methods = {
-  getRequestOptions: function(parameters, callback, options) {
-    var request_options = new Object();
-    Object.extend(request_options, Object.clone(this.default_options))
-    Object.extend(request_options, Object.clone(this.constructor.defaultOptions))    
-    request_options.parameters = Object.extend(Object.clone(request_options.parameters), parameters)    
-    request_options.onSuccess = callback;
-    if (options) {
-      Object.extend(request_options.parameters, options.parameters)
-      delete options.parameters        
-      Object.extend(request_options, options)
+  Class: {
+    class_default_options: {},
+    
+    create: function(class_name, options, class_default_options) {
+      options = (options || {});
+
+      var new_class = this[class_name] = Class.create(this, {
+        default_options: options,
+        
+        initialize: function($super, url, parameters, callback, options) {
+          $super(url, this.getRequestOptions(parameters, callback, options));          
+        }
+      }).addMethods(Ajax.Party.Util.Methods.Instance);
+
+      Object.extend(new_class, Ajax.Party.Util.Methods.Class);
+      Object.extend(new_class.class_default_options, class_default_options);
+      return new_class;  
     }
-    return request_options;
-  }
+  },
   
+  Instance: {
+    getRequestOptions: function(parameters, callback, options) {
+      var request_options = {};
+      Object.extend(request_options, Object.clone(this.default_options))
+      Object.extend(request_options, Object.clone(this.constructor.class_default_options))    
+      request_options.parameters = Object.extend(Object.clone(request_options.parameters), parameters)    
+      request_options.onSuccess = callback;
+      if (options) {
+        Object.extend(request_options.parameters, options.parameters)
+        delete options.parameters        
+        Object.extend(request_options, options)
+      }
+      return request_options;
+    }
+  }
 };
+
+Object.extend(Ajax.Party, Ajax.Party.Util.Methods.Class);
 
 Ajax.Party.create('Post');
 Ajax.Party.create('Get', { method: 'get'});
